6fb9e311b34e5d6a7cdf50fa239570652b8ec842
Melissa Rice <mmrice22@gmail.com>
2021-06-01 15:36:05 +0000

    random number is generated

--- Game.java
+++ Game.java
@@ -1,9 +1,5 @@
-/*
-
-A number-guessing game.
-
-*/
 import java.util.Scanner;
+import java.util.Random;
 
 
 public class Game {
@@ -15,6 +11,13 @@ public class Game {
     String playerName = input.nextLine();
     System.out.println(playerName + ", I'm thinking of a number between 1 and 100. Try to guess the number!");
     // choose random number between 1 and 100
+    // Create instance of the Random Class
+    Random rand = new Random();
+    // call method nextInt and pass in the upper range bound
+    int number = rand.nextInt(100);
+    // print the random number to see if it is working
+    // System.out.println(number);
+
   }
 
 }
