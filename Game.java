0dcfb8978e1761ef9d45fb2fcf665d38d11ca662
Melissa Rice <mmrice22@gmail.com>
2021-06-01 18:32:50 +0000

    checks that number is within range

--- Game.java
+++ Game.java
@@ -9,8 +9,8 @@ public class Game {
     System.out.println("Hi there! What is your name?");
     // get player name
     String playerName = input.nextLine();
-    System.out.println(playerName + ", I'm thinking of a number between 1 and 100. Try to guess the number!");
-    // choose random number between 1 and 100
+    System.out.println(playerName + ", I'm thinking of a number between 0 and 100. Try to guess the number!");
+    // choose random number between 0 and 100
     // Create instance of the Random Class
     Random rand = new Random();
     // call method nextInt and pass in the upper range bound
@@ -30,6 +30,13 @@ public class Game {
     // to 0 everytime it goes through the loop. Counter has to be OUTSIDE the loop
     // so it doesn't start over every iteration
 
+    // if guess is OUTSIDE the range 0 to 100
+    if (guess < 0 | guess > 100) {
+      System.out.println("Ummmm..." + guess + " is not within the range we agreed upon " + playerName + "! Come on now...");
+      counter = counter + 1;
+      continue;
+    }
+
     if (guess < number) {
       System.out.println("Your guess is too low, try again!");
       counter = counter + 1;
@@ -44,10 +51,6 @@ public class Game {
       System.out.println("Well done, " + playerName + "! You found my number in " + counter + " tries!");
       break;
     }
-
     }
-
-
   }
-
 }
